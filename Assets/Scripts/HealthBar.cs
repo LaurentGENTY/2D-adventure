740884c94f837a1f31c7492f760e48eb516bdefe
LaurentGENTY <laurentgenty1@gmail.com>
2020-09-20 19:48:12 +0200

    scripts health player

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public Gradient gradient;
+    public Image fill;
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        SetHealth(health);
+
+        fill.color = gradient.Evaluate(1f);
+
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
