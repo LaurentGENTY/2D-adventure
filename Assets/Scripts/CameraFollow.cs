a7892d744254c88100571273ba03e028a0dbc7aa
LaurentGENTY <laurentgenty1@gmail.com>
2020-09-20 12:29:48 +0200

    First import

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    public float timeOffset;
+    public Vector3 posOffset;
+
+    private Vector3 velocity;
+
+    void Update()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + posOffset, ref velocity, timeOffset);
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMouvement.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerMouvement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    public Rigidbody2D rb;
+
+    private bool isJumping;
+    private bool isGrounded;
+
+    public Transform groundCheckLeft;
+    public Transform groundCheckRight;
+
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+
+    private Vector3 velocity = Vector3.zero;
+
+    void FixedUpdate()
+    {
+
+        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
+
+        float horizontalMouvement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            isJumping = true;
+
+        MovePlayer(horizontalMouvement);
+
+        Flip(rb.velocity.x);
+
+        float characterVelocity = Mathf.Abs(rb.velocity.x);
+        animator.SetFloat("Speed", characterVelocity);
+    }
+
+    private void MovePlayer(float _horizontalMouvement)
+    {
+        Vector3 targetVelocity = new Vector3(_horizontalMouvement, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, 0.05f);
+
+        if (isJumping) {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isJumping = false;
+        }
+    }
+
+    public void Flip(float velocity)
+    {
+        if (velocity > 0.1f)
+            spriteRenderer.flipX = false;
+        else if (velocity < -0.1f)
+            spriteRenderer.flipX = true;
+    }
+}
