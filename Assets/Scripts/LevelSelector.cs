09e1e7a5660a454ebd8226486a470ea231932b43
LaurentGENTY <laurentgenty1@gmail.com>
2020-09-25 02:19:36 +0200

    script de choix de niveau

---
+++ Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelector : MonoBehaviour
+{
+    public Button[] levels;
+
+    public void Start()
+    {
+        int levelReached = PlayerPrefs.GetInt("levelReached", 1);
+
+        /* Griser les boutons de base */
+        for (int i = 0; i < levels.Length; i++)
+            if (i + 1 > levelReached)
+                levels[i].interactable = false;
+    }
+
+    public void LoadLevelPassed(string levelName)
+    {
+    
+        SceneManager.LoadScene(levelName);
+    }
+}
