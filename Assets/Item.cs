872ed617f9243a2aa3bf1c5bd62d462dc4303c17
LaurentGENTY <laurentgenty1@gmail.com>
2020-09-26 19:22:42 +0200

    scriptable object pour l'inventaire : ajout des potions

---
+++ Assets/Item.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "Inventory/Item")]
+public class Item : ScriptableObject
+{
+    public int id;
+    public string name;
+    public string description;
+    public Sprite image;
+    public int hpGiven;
+    public int speedGiven;
+    public float speedDuration;
+  
+}
